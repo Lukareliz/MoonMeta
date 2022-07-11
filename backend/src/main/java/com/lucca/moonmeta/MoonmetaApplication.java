2953080dfdabf814290c4ad90be459740fe1c0eb
Lucca Nunes <lyulucca@gmail.com>
2022-07-11 17:51:49 -0300

    Project Created

---
+++ backend/src/main/java/com/lucca/moonmeta/MoonmetaApplication.java
@@ -0,0 +1,13 @@
+package com.lucca.moonmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonmetaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucca/moonmeta/MoonmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucca.moonmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
